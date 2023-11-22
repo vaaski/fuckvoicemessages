71c3282804fcb82ae8808badf280aff4d4f90927
vaaski <admin@vaa.ski>
2023-11-22 15:12:35 +0100

    telegram bot

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,13 @@
-console.log("Hello, world!")
-export{}
+import { Bot } from "grammy"
+
+const { TELEGRAM_BOT_TOKEN } = process.env
+if (!TELEGRAM_BOT_TOKEN) throw new Error("TELEGRAM_BOT_TOKEN is not defined")
+
+const bot = new Bot(TELEGRAM_BOT_TOKEN)
+
+bot.on("message:text", ctx => ctx.reply("Echo: " + ctx.message.text))
+
+console.log("Starting bot...")
+await bot.start()
+
+export {}
