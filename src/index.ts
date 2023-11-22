240afe6f479fe74f90e8b50bf62e51f58ddca7e5
vaaski <admin@vaa.ski>
2023-11-22 16:42:44 +0100

    transcribe

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,44 @@
-import { Bot, InputFile } from "grammy"
+import { FileFlavor, hydrateFiles } from "@grammyjs/files"
+import { Bot, Context, InputFile } from "grammy"
 import OpenAI from "openai"
 
 const { TELEGRAM_BOT_TOKEN, OPENAI_API_KEY } = process.env
 if (!TELEGRAM_BOT_TOKEN) throw new Error("TELEGRAM_BOT_TOKEN is not defined")
 if (!OPENAI_API_KEY) throw new Error("OPENAI_API_KEY is not defined")
 
-const bot = new Bot(TELEGRAM_BOT_TOKEN)
+type MyContext = FileFlavor<Context>
+const bot = new Bot<MyContext>(TELEGRAM_BOT_TOKEN)
+bot.api.config.use(hydrateFiles(bot.token))
+
 const openai = new OpenAI({ apiKey: OPENAI_API_KEY })
 
+bot.command("start", ctx => {
+  ctx.reply(
+    "Send text and I'll read it out loud. Send a voice message and I'll transcribe it."
+  )
+})
+
+bot.api.setMyCommands([{ command: "/start", description: "Send the start text again" }])
+
+bot.on("message", async (ctx, next) => {
+  const { username, first_name, last_name, id } = ctx.msg.from
+  const name = username ?? `${first_name} ${last_name}`
+  console.log(`new message from [${id}] ${name}`)
+
+  if (ctx.msg.text) console.log(`  ${ctx.msg.text}`)
+  else if (ctx.msg.voice) console.log(`  [VOICE] ${ctx.msg.voice.duration}s`)
+  else console.log(`  something fucked`)
+
+  next()
+})
+
 bot.on("message:text", async ctx => {
-  console.log(ctx.message.text)
-  const waitingMessage = ctx.reply("generating...")
+  const { text } = ctx.message
+
+  const waitingMessage = ctx.reply("Generating...")
 
   const audio = await openai.audio.speech.create({
-    input: ctx.message.text,
+    input: text,
     model: "tts-1",
     voice: "onyx",
     response_format: "opus",
@@ -25,7 +50,25 @@ bot.on("message:text", async ctx => {
   await ctx.replyWithVoice(new InputFile(audio.body))
 })
 
+bot.on("message:voice", async ctx => {
+  const voice = ctx.msg.voice
+
+  const duration = voice.duration
+  const waitingMessage = ctx.reply(`Transcribing your ${duration}s voice message.`)
+
+  const file = await ctx.getFile()
+
+  const response = await fetch(file.getUrl())
+  if (!response.body) throw new Error("No response body")
+
+  const transcription = await openai.audio.transcriptions.create({
+    file: response,
+    model: "whisper-1",
+  })
+
+  bot.api.deleteMessage(ctx.chat.id, (await waitingMessage).message_id)
+  await ctx.reply(transcription.text)
+})
+
 console.log("Starting bot...")
 await bot.start()
-
-export {}
